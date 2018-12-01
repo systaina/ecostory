0232c61b4039fe51db0ff044379663d32ee7a8f3
omarcodex <omaralimalik@gmail.com>
2018-12-01 14:38:41 -0800

    Add skeleton for site and app

---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  root 'home#index' # This sets the home/welcome page
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
+end
